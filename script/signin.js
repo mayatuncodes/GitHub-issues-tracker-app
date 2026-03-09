d0f0b87e12bf8f1639fae4a9fc3c356c33709d34
mayatuncodes <mayatuncodes@gmail.com>
2026-03-09 18:15:50 +0600

    Added dashboard UI And Modify Js file

--- script/signin.js
+++ script/signin.js
@@ -11,23 +11,11 @@ document.addEventListener("DOMContentLoaded", function () {
     const username = usernameInput.value;
     const password = passwordInput.value;
 
-    if (username === "admin" && password === "admin123") {
-      successMsg.classList.remove("hidden");
-
-      setTimeout(() => {
-        window.location.href = "dashboard.html";
-      }, 1500);
+      if (username === "admin" && password === "admin123") {
+      alert("Login Successful");
+      window.location.href = "dashboard.html";
     } else {
       alert("Login Failed");
-    }
-  });
-
-  // Enter press anywhere in form fills auto credentials
-  form.addEventListener("keydown", function (e) {
-    if (e.key === "Enter") {
-      e.preventDefault(); // Prevent default submit
-      usernameInput.value = "admin";
-      passwordInput.value = "admin123";
     }
   });
 });
